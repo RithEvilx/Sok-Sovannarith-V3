6a4f72950b13d132ed2db1959a29880ecaf1199a
TempEvil <tempevilx@gmail.com>
2026-02-17 11:13:09 +0700

    finishing the final touch

--- src/constants/data/BeyondScreenData.ts
+++ src/constants/data/BeyondScreenData.ts
@@ -1,8 +1,8 @@
 import type { BeyondScreenDataType } from "@/types/BeyondScreenDataType";
-import { FirstPortfolioImg, HoverCardEffectImg, RomdoulKravanhResortImg } from "../app";
+import { DummyImg01, DummyImg02, DummyImg03 } from "../app";
 
 export const BeyondScreenData: BeyondScreenDataType = {
   description:
     "Outside of work, I focus on staying active. I love inline skating, FPV drones, and sim racing. These hobbies help me clear my head and stay energized for my development projects.",
-  gallery: [RomdoulKravanhResortImg, HoverCardEffectImg, FirstPortfolioImg],
+  gallery: [DummyImg01, DummyImg02, DummyImg03],
 };
